081dea40e58bab63e85b47ab42ce7deb11208002
matt0418 <mfeldma7@gmail.com>
2019-04-28 00:21:33 -0400

    auth router setup

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -10,4 +10,57 @@ router.get('/', async (req, res) => {
     return res.status(200).json({ message: "auth router is up" })
 })
 
+router.post('/register', async (req, res) => {
+    let user = req.body
+
+    if (!user.username || !user.password || !user.name || !user.email) {
+        return res.status(400).json({ message: "Please provide all fields" })
+    }
+
+    const hash = bcrypt.hashSync(user.password, 12)
+    user.password = hash
+    try {
+        const newUser = await Users.add(user)
+        return res.status(201).json(newUser)
+    } catch(error) {
+        console.log(error)
+        return res.status(500).json(error)
+    }
+})
+
+router.post('/login', async (req, res) => {
+    let { username, password } = req.body
+    if (username && password) {
+        try {
+            const loggedIn = await Users.findBy({ username }).first()
+            if (loggedIn && bcrypt.compareSync(password, loggedIn.password)) {
+                const token = generateToken(loggedIn)
+                return res.status(200).json({
+                    message: `Hi ${loggedIn.name}`,
+                    token,
+                    id: loggedIn.id
+                })
+            }
+        } catch(error) {
+            console.log(error)
+            res.status(500).json(error)
+        }
+    } else {
+        return res.status(400).json({ message: "Please provide both username and password" })
+    }
+})
+
+function generateToken(user) {
+    const payload = {
+        subject: user.id,
+        username: user.username,
+        name: user.name,
+        role: user.role,
+    }
+    const options = {
+        expiresIn: '1d'
+    }
+    return jwt.sign(payload, secrets.jwtSecret, options)
+}
+
 module.exports = router
